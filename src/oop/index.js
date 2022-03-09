c916cedd862626a4cc0bb1ec5230bf7fffa77503
yahya boyalı <yahyaboyali@gmail.com>
2022-03-09 14:27:59 +0300

    base class prototyping child class super kullanım

--- src/oop/index.js
+++ src/oop/index.js
@@ -1,7 +1,12 @@
 // değerleri atamak için prototip oluşturuyoruz 
 class Customer {
     constructor(id,customerNumber){
-
+        // buraya direk aslında prototyping yapıyoruz 
+        // özellik eklemiş oluyoruz bu sayede aşağıda 
+        // id ve customerNumber ı okuyabiliyoruz
+        // saf js de class yok fonksiyon var
+        this.id=id
+        this.customerNumber=customerNumber
     }
 }
 
@@ -16,3 +21,21 @@ console.log(customer.name)
 // direk Classın kendisine prototyping 
 Customer.bisey="bisey"
 console.log(Customer.bisey)
+
+// mantığın babası geliyor burası çok önemli sürekli bakmalısın
+
+// superi kullanıp yukarının constructoru çalıştırıldı
+class BireyselCustomer extends Customer{
+    constructor(firstname,id,customerNumber){
+        super(id,customerNumber)// ana classın constructoruna gönderdik
+        this.firstname=firstname// prototyping yapıldı
+    }
+
+}
+
+class KurumsalCustomer extends Customer{
+    constructor(companyName,id,customerNumber){
+        super(id,customerNumber)
+        this.companyName=companyName
+    }
+}
